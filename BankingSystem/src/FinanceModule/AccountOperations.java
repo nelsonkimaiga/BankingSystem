2d0a615bcf5a900051c47d64f2fd0a014203b21f
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-09-30 01:41:05 +0300

    Look and feel added

--- BankingSystem/src/FinanceModule/AccountOperations.java
+++ BankingSystem/src/FinanceModule/AccountOperations.java
@@ -6,6 +6,8 @@
 
 package FinanceModule;
 import javax.swing.UIManager;
+import java.sql.*;
+
 
 /**
  *
@@ -71,6 +73,11 @@ public class AccountOperations extends javax.swing.JFrame {
 
         jButton1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jButton1.setText("SEARCH");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -158,6 +165,10 @@ public class AccountOperations extends javax.swing.JFrame {
 new ModuleDashboard().setVisible(true);
 dispose();// TODO add your handling code here:
     }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
